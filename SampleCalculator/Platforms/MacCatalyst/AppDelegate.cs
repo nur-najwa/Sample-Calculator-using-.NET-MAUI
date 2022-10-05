c533e7fef0d93f66f638364f63b44cdd4bfa37b0
Najwa <najwa@iris.com.my>
2022-10-05 16:38:46 +0800

    first commit

---
+++ SampleCalculator/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SampleCalculator;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
